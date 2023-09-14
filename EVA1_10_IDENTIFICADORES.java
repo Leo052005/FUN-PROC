cf632336271db7b5908e99e20c63bd5957565eae
Leo052005 <143568859+Leo052005@users.noreply.github.com>
2023-09-14 12:52:21 -0700

    Add files via upload

---
+++ EVA1_10_IDENTIFICADORES.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_10_identificadores;
+
+/**
+ *
+ * @author Ezequiel Leonardo Aguilar Llanes
+ */
+public class EVA1_10_IDENTIFICADORES {
+
+    public static void main(String[] args) {
+     // TODO code application logic here
+     //int 1salario; no cumple con las reglas: inicia con numero 
+     
+     int mi_salario;
+     int $salario;
+     int salario1000;
+     //int _; no es valido
+     int $;
+     //int mi salario; no es valido, espacio en blanco
+     
+     int salario;
+     int Salario;
+     int SALARIO;
+     //int salario; no podemos repetir identificadores 
+     
+     //int public; no podemos usar keywords (palabras reserbadas)
+     
+     int println; 
+    }
+}

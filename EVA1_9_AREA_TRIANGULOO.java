e1e66ae09940becdafe885a776d8406b01b1c8fb
Leo052005 <143568859+Leo052005@users.noreply.github.com>
2023-09-14 13:28:06 -0700

    Add files via upload

---
+++ EVA1_9_AREA_TRIANGULOO.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_9_area_trianguloo;
+
+/**
+ *
+ * @author Ezequiel Leonardo Aguilar Llanes
+ */
+public class EVA1_9_AREA_TRIANGULOO {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //double es el tipo rel (para numeros con parte decimal)
+        double base;
+        double altura;
+        double area;
+        scanner captu = new scaner (system.in);
+        
+        syste.out.println("introduce la base del triangulo:");
+        base = captu.nexDouble();
+        system.out.println("introduce la altura del triangulo");
+        altura = captu.nexDouble();
+        
+        system.out.printlin("introduce la base del tringulo:");
+        base = captu.nextDouble();
+        system.out.println("inttoduce la altura del triangulo");
+        altura = captu.nexDouble();
+        
+        area = (base * altura) / 2
+        system.out.println("El area del triangulo es:");
+        System.out.println(area);
+        
+    }
+}

3150de06bf99632e720994a5c5e0d9841fe428e4
Leo052005 <143568859+Leo052005@users.noreply.github.com>
2023-10-27 12:29:16 -0600

    Add files via upload

---
+++ EVA2_13_CICLO_FOR1.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_13_ciclo_for1;
+
+/**
+ *
+ * @author leo
+ */
+public class EVA2_13_CICLO_FOR1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            
+        }
+        System.out.println("-----------------------------------------------------");
+        for (int i = 9; i >= 0; i--) {
+            System.out.println("i");
+            
+        }
+        for (int i = 0; i <= 100; i+=2) {
+            System.out.println(i + "-");
+            
+            
+        }
+    }
+}
+    
---
+++ EVA2_14_CICLO_FOR_2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_14_ciclo_for_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author leo
+ */
+public class EVA2_14_CICLO_FOR_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int noCalifas;
+        Scanner input = new Scanner (System.in);
+        System.out.println("Introduce el numero de calificaciones");
+        noCalifas = input.nextInt();
+        for (int i = 0; i < noCalifas; i++) {
+            System.out.println("Intoduce la calificacion");
+            int califa = input.nextInt();
+            
+            
+        }
+        
+                
+    }
+    
+}

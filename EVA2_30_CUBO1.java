28a8fbe260119c593183a6f7991622e52a1b721a
Leo052005 <143568859+Leo052005@users.noreply.github.com>
2023-11-22 09:35:01 -0600

    Add files via upload

---
+++ EVA2_30_CUBO1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_30_cubo1;
+
+/**
+ *
+ * @author leo
+ */
+public class EVA2_30_CUBO1 {
+
+    public static void main(String[] args) {
+        int cubo[][][] = new int[3][4][5];
+        
+        for (int i = 0; i < cubo.length; i++) {
+            for (int j = 0; j < cubo[i].length; j++) {
+                for (int k = 0; k < 5; k++) {
+                    System.out.println("[" + cubo[i][j][k] +"]");
+                System.out.println("");    
+                System.out.println("");
+                    
+                }
+                
+            }
+            
+        }
+    }
+}
